c75dc2d8844f50bd671c8288e8f1a05f0f896776
merlindwiar <merlindwiar29@gmail.com>
2020-04-20 13:25:41 +0700

    membuat 2 tampilan wadah dan tutupnya

--- script.js
+++ script.js
@@ -4,17 +4,40 @@ var ctx=canvas.getContext('2d');
 canvas.width=canvas.scrollWidth;
 canvas.height=canvas.scrollHeight;
 
-//create gradient
-// var grd=ctx.createLinierGradient();
-// grd.addColorStop(0,"#00ff99");
-// grd.addColorStop(1,"#003399");
-
-
-ctx.beginPath();
-ctx.moveTo(630,145);
-ctx.lineTo(630,145);
-ctx.lineTo(720,145);
-ctx.lineTo(720,275);
-ctx.lineTo(630,275);
+//bawah1
 ctx.fillStyle='#000035';
-ctx.fill();
+ctx.fillRect(60,265,100,150);
+
+//tutup1
+ctx.fillStyle='yellow';
+ctx.fillRect(60,245,99,20);
+ctx.strokeStyle="black";
+ctx.lineWidth=1;
+ctx.strokeRect(61,245,98,20);
+
+//pegangan1
+ctx.fillStyle='yellow';
+ctx.fillRect(103,235,10,10);
+ctx.strokeStyle="black";
+ctx.lineWidth=1;
+ctx.strokeRect(103,235,10,10);
+
+//bawah2
+ctx.fillStyle='#000035';
+ctx.fillRect(250,265,100,150);
+
+//tutup2
+ctx.fillStyle='yellow';
+ctx.translate(273,-127);
+ctx.rotate(75 * Math.PI/360);
+ctx.fillRect(200,245,99,20); 
+ctx.strokeStyle="black";
+ctx.lineWidth=1;
+ctx.strokeRect(200,245,99,20); 
+
+//pegangan2
+ctx.fillStyle='yellow';
+ctx.fillRect(245,235,10,10); 
+ctx.strokeStyle="black";
+ctx.lineWidth=1;
+ctx.strokeRect(245,235,10,10); 
